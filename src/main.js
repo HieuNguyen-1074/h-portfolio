522f73a756e134c3ee92fdca883da48bdfbc5174
HieuNguyen-1074 <hjeu1074@gmail.com>
2020-09-04 21:37:23 +0700

    1

--- src/main.js
+++ src/main.js
@@ -20,28 +20,46 @@ function loading() {
 function displayInforMore() {
     let rote = true;
     $('.header-icon__menu').click(() => {
+        $(".informore").toggleClass("active");
+
+
         if (rote) {
+            $('.header-icon__menu').css("transform", "rotate(720deg)");
+            $('.header').children('h1').css("color", "#fff");
             for (let x = 0; x < 5; x++) {
 
                 $('.header-icon__menu').children("div").eq(x).css("opacity", "0");
-                $('.header-icon__menu').css("transform", "rotate(720deg)");
+                $('.header-icon__menu').children("div").eq(x).css("background-color", "#fff");
                 if (x === 3 || x == 4) {
                     $('.header-icon__menu').css("transform", "rotate(720deg)");
                     $('.header-icon__menu').children("div").eq(x).css("opacity", "1");
+                    $('.header-icon__menu').children("div").eq(x).css("background-color", "#fff");
 
                 }
+                setTimeout(() => {
+                    $('.informore-introduce').children().addClass('active');
+                    $('.informore-contact').children().children().addClass('active');
+                    $('.informore-link-container').addClass('active');
+                }, 1100);
                 rote = false;
             }
         } else {
+            $('.header-icon__menu').css("transform", "rotate(720deg)");
+            $('.header').children('h1').css("color", "black");
             for (let x = 0; x < 5; x++) {
 
                 $('.header-icon__menu').children("div").eq(x).css("opacity", "1");
-                $('.header-icon__menu').css("transform", "rotate(720deg)");
+                $('.header-icon__menu').children("div").eq(x).css("background-color", "black");
+
                 if (x === 3 || x == 4) {
                     $('.header-icon__menu').css("transform", "rotate(0deg)");
                     $('.header-icon__menu').children("div").eq(x).css("opacity", "0");
+                    $('.header-icon__menu').children("div").eq(x).css("background-color", "black");
 
                 }
+                $('.informore-introduce').children().removeClass('active');
+                $('.informore-contact').children().children().removeClass('active');
+                $('.informore-link-container').removeClass('active');
                 rote = true;
             }
         }
@@ -53,8 +71,16 @@ function displayInforMore() {
 
 function main() {
     loading();
-    displayInforMore()
+    displayInforMore();
+
 }
 main();
 
 $("body").children("div").not(".loading").css("opacity", "0");
+$("body").on('mousewheel', (e) => {
+    if (e.originalEvent.wheelDelta / 120 > 0) {
+
+    } else {
+        console.log('scrolling down !');
+    }
+})
